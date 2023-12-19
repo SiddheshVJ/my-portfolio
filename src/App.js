0ea301e75dc56637c90b8ad677096a7b1984af83
Siddhesh Jadhav <siddheshvjadhav20@gmail.com>
2023-12-19 22:52:46 +0530

    Header, HOme, Social, Data - added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
 import './App.css';
-
+import Header from '../src/components/header/Header'
+import Home from './components/home/Home';
 function App() {
   return (
     <>
-      app
+      <Header />
+      <main className='main'>
+        <Home/>
+      </main>
     </>
   );
 }
---
+++ src/components/home/Social.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <>
+            <div className="hoome__socila">
+                <a href="" className="home__social-icon" target='_blank'>
+                    <i className="uil uil-instagram"></i>
+                </a>
+                <a href="" className="home__social-icon" target='_blank'>
+                    <i className="uil uil-dribbble"></i>
+                </a>
+                <a href="" className="home__social-icon" target='_blank'>
+                    <i className="uil uil-github-alt"></i>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Social
