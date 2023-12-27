a842d76b8269d49468a9eb819ae0cd6cc03d0152
Siddhesh Jadhav <siddheshvjadhav20@gmail.com>
2023-12-27 16:53:16 +0530

    About mw and skills page added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from '../src/components/header/Header'
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+
+
+
 function App() {
   return (
     <>
       <Header />
       <main className='main'>
-        <Home/>
+        <Home />
+        <About />
+        <Skills />
+
       </main>
     </>
   );
